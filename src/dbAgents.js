87661a2c1e103047c11267cd14cc1a9739a39d80
ClauditeNunc <justme_2021@mail.ru>
2024-01-07 00:12:53 +0100

    Initial commit

--- src/dbAgents.js
+++
@@ -1,8 +0,0 @@
-const agents = [
-    { name: "Olivia Reynolds", designation: "Senior Real Estate Consultant", imageURL: './imgs/team-1.jpg', index: 1 },
-    { name: "Elijah Mitchell", designation: "Executive Property Advisor", imageURL: './imgs/team-2.jpg', index: 2 },
-    { name: "Sophie Hernandez", designation: "Residential Property Specialist", imageURL: './imgs/team-3.jpg', index: 3 },
-    { name: "Cameron Knight", designation: "Commercial Real Estate Manager", imageURL: './imgs/team-4.jpg', index: 4 },
-];
-
-export default agents;
