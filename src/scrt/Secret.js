0e85769c4d56f1d6fd321dc5ed479ad4b9faff04
ClauditeNunc <justme_2021@mail.ru>
2024-01-09 18:13:48 +0100

    bug fixing

--- src/scrt/Secret.js
+++ src/scrt/Secret.js
@@ -5,7 +5,9 @@ function ForJ(){
                 padding:"20%",
                 fontStyle:"Inter",
                 fontWeight:"bold",
-                backgroundColor:"#efa9b8"
+                backgroundColor:"#efa9b8",
+                fontSize:"4rem",
+                color:"beige"
             }}>
                 If you've stumbled upon it randomly, that's funny, but it's not for you.
                 However if you are who this is meant for, I just wanted to tell you that I love you and in the end the whole point of this website is so that I can get to you one day.
