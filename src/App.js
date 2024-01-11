515ff7a3ff14d7ec0da5946e74c96659e61d9a79
ClauditeNunc <justme_2021@mail.ru>
2024-01-11 17:08:42 +0100

    bug fixing

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import ClientReviews from './ClientReviews/ClientReviews';
 import Footer from './Footer/Footer';
 import Testimonial from './Testimonial/Testimonial';
 import Error404 from './Error404/Error404';
-import ForJ from './scrt/Secret';
 import AddProperty from './AddProperty/AddProperty';
 import { propertyLocations, propertyTypes, databaseOfPseudoProperties as initialDB, databaseOfPseudoProperties } from './locations';
 import Cookies from './Cookies/Cookies';
@@ -60,7 +59,6 @@ function App() {
         <Route path="/help" element={<Help />} />
         <Route path="/faq" element={<Faq />} />
         <Route path="/property-listing-extra" element={<PropertyListingExtra />} />
-        <Route path="/myDear" element={<ForJ/>} />
         <Route
           path="/*"
           element={<Error404 />}
